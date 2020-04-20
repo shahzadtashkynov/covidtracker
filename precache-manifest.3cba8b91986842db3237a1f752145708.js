6ac53c3cea0ed9ff231fb1d2a40bd78d71bdc861
shahzadtashkynov <shahzad261095@gmail.com>
2020-04-20 18:15:01 +0500

    Updates

---
+++ precache-manifest.3cba8b91986842db3237a1f752145708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963b1b2eb25cad2c42d093aa6aea5dcc",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "6060136c53d0a846f247",
+    "url": "/covidtracker/static/css/main.1da00e59.chunk.css"
+  },
+  {
+    "revision": "c6ddc6628fa3d99d1eb6",
+    "url": "/covidtracker/static/js/2.7bd4e690.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.7bd4e690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6060136c53d0a846f247",
+    "url": "/covidtracker/static/js/main.b9492787.chunk.js"
+  },
+  {
+    "revision": "344c7e375ba8b52e75d4",
+    "url": "/covidtracker/static/js/runtime-main.d51b74a4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
